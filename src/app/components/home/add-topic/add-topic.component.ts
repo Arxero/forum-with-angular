4352c916d361fa7756dbb093b0b0b56ba2c4bb2e
Maverick Cloud <admin@syntrwave.com>
2019-01-15 00:07:34 +0200

    added preview option to work on new topic and edit topic

--- src/app/components/home/add-topic/add-topic.component.ts
+++ src/app/components/home/add-topic/add-topic.component.ts
@@ -13,7 +13,6 @@ import { UserModel } from 'src/app/core/models/auth-models/user.model';
 })
 export class AddTopicComponent implements OnInit {
     addTopicModel: AddTopicModel
-    alteredDescription: string
     forumId: string
     forumName: Object = {
         '1': 'Announcements',
@@ -23,6 +22,8 @@ export class AddTopicComponent implements OnInit {
         '5': 'Cafeteria',
     }
     user: UserModel
+    alteredDescription: string
+    urlRegex: any = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g)
 
     constructor(
         private topicService: TopicService,
@@ -52,29 +53,56 @@ export class AddTopicComponent implements OnInit {
     }
 
     convertBBCode() {
-        //console.log('yes');
-        console.log(this.alteredDescription);
-        this.alteredDescription = ''
+        this.alteredDescription = this.addTopicModel.description
+
         //looking for new lines and if there is replace it with break
-        this.addTopicModel.description = this.addTopicModel.description.replace(new RegExp('\n', 'g'), "<br/>")
-    
+        this.alteredDescription = this.alteredDescription.replace(new RegExp('\n', 'g'), "<br/>")
+        
         //looking for urls in the post and if there is make them clickable
-        // let urlRegexNoImgTag = new RegExp(/(?<!])https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g)
-        // if (urlRegexNoImgTag.test(this.addTopicModel.description)) {
+        let urlRegexNoImgTag = new RegExp(/(?<!])https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g)
+        if (urlRegexNoImgTag.test(this.alteredDescription)) {
             
-        //     this.addTopicModel.description.match(urlRegexNoImgTag).map(url => {
-        //         this.alteredDescription = this.addTopicModel.description.replace(url, `<a href=${url}>${url}</a>`)
-        //     })
-        // }
+            this.alteredDescription.match(urlRegexNoImgTag).map(url => {
+                this.alteredDescription = this.alteredDescription.replace(url, `<a href=${url}>${url}</a>`)
+            })
+        }
 
         //looking for bbcode for image and if there is one or more, display the image
         let imageRegexWithTags = new RegExp(/\[img]https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)\[\/img]/g)
         let imageRegexWithoutTags = new RegExp(/(?<=\[img])https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)(?=\[\/img])/g)
-        if (imageRegexWithTags.test(this.addTopicModel.description)) {
-            this.addTopicModel.description.match(imageRegexWithTags).map(imageUrlWithTags => {
+        if (imageRegexWithTags.test(this.alteredDescription)) {
+            this.alteredDescription.match(imageRegexWithTags).map(imageUrlWithTags => {
                 let imageUrlWithoutTags = imageUrlWithTags.match(imageRegexWithoutTags)[0]
                 
-                this.addTopicModel.description = this.addTopicModel.description.replace(imageUrlWithTags, `<img src="${imageUrlWithoutTags}" alt="image" class="post-image">`)
+                this.alteredDescription = this.alteredDescription.replace(imageUrlWithTags, `<img src="${imageUrlWithoutTags}" alt="image" class="post-image">`)
+            })
+        }
+
+        //looking for video link ending like .mp4
+        let videoTagRegexWithTags =  new RegExp(/\[video]https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)\[\/video]/g)
+        if (videoTagRegexWithTags.test(this.alteredDescription)) {
+            this.alteredDescription.match(videoTagRegexWithTags).map(videoUrlWithTags => {
+                let videoUrlWithoutTags = videoUrlWithTags.match(this.urlRegex)[0]
+                this.alteredDescription = this.alteredDescription.replace(videoUrlWithTags, `<video src="${videoUrlWithoutTags}" controls="controls" width=100%>Please upgrade to a browser which supports HTML 5.</video>`)
+            })
+        }
+
+        //looking for audio links ending like .mp3
+        let audioTagRegexWithTags =  new RegExp(/\[audio]https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)\[\/audio]/g)
+        if (audioTagRegexWithTags.test(this.alteredDescription)) {
+            this.alteredDescription.match(audioTagRegexWithTags).map(audioUrlWithTags => {
+                let audioUrlWithoutTags = audioUrlWithTags.match(this.urlRegex)[0]
+                this.alteredDescription = this.alteredDescription.replace(audioUrlWithTags, `<audio src="${audioUrlWithoutTags}" controls="controls" width=100%>Please upgrade to a browser which supports HTML 5.</audio>`)
+            })
+        }
+
+        //looking for youtube url in order to embed the video directly
+        let youtubeUrlRegex = new RegExp(/(?<=(<a href=https:\/\/www\.youtube\.com\/watch\?v=).{11}>(https:\/\/www\.youtube\.com\/watch\?v=)).*?(?=<\/a>)/g)
+        if (youtubeUrlRegex.test(this.alteredDescription)) {
+            this.alteredDescription.match(youtubeUrlRegex).map(videoId => {
+                let youtubeLinkWithAnchors = `<a href=https://www.youtube.com/watch?v=${videoId}>https://www.youtube.com/watch?v=${videoId}</a>`
+                let videoUrl = 'https://www.youtube.com/embed/' + videoId;
+                this.alteredDescription = this.alteredDescription.replace(youtubeLinkWithAnchors, `<iframe max-width="100%" width="100%" height="423" src="${videoUrl}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`)
             })
         }
     }
--- src/app/components/home/edit-topic/edit-topic.component.ts
+++ src/app/components/home/edit-topic/edit-topic.component.ts
@@ -12,15 +12,17 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class EditTopicComponent implements OnInit {
     editTopicModel: AddTopicModel
-    forumId : number
-    topicId : string
+    forumId: number
+    topicId: string
     forumName: Object = {
-        '1' : 'Announcements',
-        '2' : 'VIP Application',
-        '3' : 'Admin Application',
-        '4' : 'Suggestions',
-        '5' : 'Cafeteria',
+        '1': 'Announcements',
+        '2': 'VIP Application',
+        '3': 'Admin Application',
+        '4': 'Suggestions',
+        '5': 'Cafeteria',
     }
+    alteredDescription: string
+    urlRegex: any = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g)
 
     constructor(
         private topicService: TopicService,
@@ -38,13 +40,70 @@ export class EditTopicComponent implements OnInit {
         })
     }
 
-    editTopic(){
+    editTopic() {
         this.topicService.editTopic(this.topicId, this.editTopicModel).subscribe(() => {
             this.toastr.success('Topic edited successfully', 'Success')
             this.router.navigate([`view/forum/${this.forumId}/viewtopic/${this.topicId}`])
-            
+
         })
     }
+
+    convertBBCode() {
+        this.alteredDescription = this.editTopicModel.description
+
+        //looking for new lines and if there is replace it with break
+        this.alteredDescription = this.alteredDescription.replace(new RegExp('\n', 'g'), "<br/>")
+        
+        //looking for urls in the post and if there is make them clickable
+        let urlRegexNoImgTag = new RegExp(/(?<!])https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g)
+        if (urlRegexNoImgTag.test(this.alteredDescription)) {
+            
+            this.alteredDescription.match(urlRegexNoImgTag).map(url => {
+                this.alteredDescription = this.alteredDescription.replace(url, `<a href=${url}>${url}</a>`)
+            })
+        }
+
+        //looking for bbcode for image and if there is one or more, display the image
+        let imageRegexWithTags = new RegExp(/\[img]https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)\[\/img]/g)
+        let imageRegexWithoutTags = new RegExp(/(?<=\[img])https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)(?=\[\/img])/g)
+        if (imageRegexWithTags.test(this.alteredDescription)) {
+            this.alteredDescription.match(imageRegexWithTags).map(imageUrlWithTags => {
+                let imageUrlWithoutTags = imageUrlWithTags.match(imageRegexWithoutTags)[0]
+                
+                this.alteredDescription = this.alteredDescription.replace(imageUrlWithTags, `<img src="${imageUrlWithoutTags}" alt="image" class="post-image">`)
+            })
+        }
+
+        //looking for video link ending like .mp4
+        let videoTagRegexWithTags =  new RegExp(/\[video]https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)\[\/video]/g)
+        if (videoTagRegexWithTags.test(this.alteredDescription)) {
+            this.alteredDescription.match(videoTagRegexWithTags).map(videoUrlWithTags => {
+                let videoUrlWithoutTags = videoUrlWithTags.match(this.urlRegex)[0]
+                this.alteredDescription = this.alteredDescription.replace(videoUrlWithTags, `<video src="${videoUrlWithoutTags}" controls="controls" width=100%>Please upgrade to a browser which supports HTML 5.</video>`)
+            })
+        }
+
+        //looking for audio links ending like .mp3
+        let audioTagRegexWithTags =  new RegExp(/\[audio]https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)\[\/audio]/g)
+        if (audioTagRegexWithTags.test(this.alteredDescription)) {
+            this.alteredDescription.match(audioTagRegexWithTags).map(audioUrlWithTags => {
+                let audioUrlWithoutTags = audioUrlWithTags.match(this.urlRegex)[0]
+                this.alteredDescription = this.alteredDescription.replace(audioUrlWithTags, `<audio src="${audioUrlWithoutTags}" controls="controls" width=100%>Please upgrade to a browser which supports HTML 5.</audio>`)
+            })
+        }
+
+        //looking for youtube url in order to embed the video directly
+        let youtubeUrlRegex = new RegExp(/(?<=(<a href=https:\/\/www\.youtube\.com\/watch\?v=).{11}>(https:\/\/www\.youtube\.com\/watch\?v=)).*?(?=<\/a>)/g)
+        if (youtubeUrlRegex.test(this.alteredDescription)) {
+            this.alteredDescription.match(youtubeUrlRegex).map(videoId => {
+                let youtubeLinkWithAnchors = `<a href=https://www.youtube.com/watch?v=${videoId}>https://www.youtube.com/watch?v=${videoId}</a>`
+                let videoUrl = 'https://www.youtube.com/embed/' + videoId;
+                this.alteredDescription = this.alteredDescription.replace(youtubeLinkWithAnchors, `<iframe max-width="100%" width="100%" height="423" src="${videoUrl}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`)
+            })
+        }
+    }
+
+
 
     addEmoji(asd) {
         let emoji = asd.target.innerText
